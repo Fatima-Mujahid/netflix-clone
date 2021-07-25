d5a832b57e79a4dddeb3ed0fca521eed1aebc9ad
Fatima-Mujahid <65939673+Fatima-Mujahid@users.noreply.github.com>
2021-07-25 12:55:57 +0500

    feat(Row) add new row

---
+++ src/Row.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import './Row.css'
+
+export default function Row({ title, fetchURL }) {
+  const baseImageURL = 'https://image.tmdb.org/t/p';
+  const imageSize = 'w500';
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  return (
+    <div className="row">
+      {movies.map(function (movie) {
+        return (
+            <img
+              className="row__image"
+              src={`${baseImageURL}/${imageSize}/${movie.poster_path}`}
+              alt={movie.title ? movie.title : movie.name}
+            />
+        );
+      })}
+    </div>
+  );
+}
